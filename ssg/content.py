c56347b2cecd289b95e88640a63ba138d58ae222
Thuo <89732702+DThuo@users.noreply.github.com>
2023-04-24 18:38:50 +1000

    Completed the project

--- ssg/content.py
+++ ssg/content.py
@@ -27,7 +27,7 @@ class Content(Mapping):
     
     @type.setter
     def type(self, type):
-        self.data[type] = type
+        self.data["type"] = type
         
     def __getitem__(self, key):
         return self.data[key]
