3382d029bbd1a7ca9a92e59362144832007bea2e
Thuo <89732702+DThuo@users.noreply.github.com>
2023-04-24 18:06:32 +1000

    Completed the project

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,33 +1,30 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
 
-import sys
-import docutils
 from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
 
-
-
 class Parser:
-    ext = ".html"
-    extensions: List[str] = []
+    base_ext = ".html"
+    file_exts: List[str] = []
 
-    def valid_extension(self, extensions):
-        return extensions in self.extensions
+    def valid_file_exts(self, extensions):
+        return file_exts in self.file_exts
     
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
     def read(self, path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read() 
         
     def write(self, path, dest, content):
-        full_path = self.dest  / path.with_suffix(self.ext).name
-        with open(full_path) as file:
+        file_path = dest  / path.with_suffix(self.base_ext).name
+        with open(file_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
